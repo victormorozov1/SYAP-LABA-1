0ee277c2493e87e32cfa99323e11351c00577fee
victormorozov1 <vismo@bk.ru>
2023-02-17 15:53:12 +0500

    add load_congratulations

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import xlrd, xlwt
 from dataclasses import dataclass
 from settings import load_settings
-import win32com.client
 
 
 def load_sheets(filename):
@@ -14,12 +13,28 @@ def load_sheets(filename):
     return sheets
 
 
+def load_congratulations(congratulations_sheets):
+    congratulation_groups = {}
+    for sheet in congratulations_sheets:
+        congratulation_groups[sheet.name] = []
+        for i in range(sheet.nrows):
+            for j in range(sheet.ncols):
+                congratulation = sheet.cell(rowx=i, colx=j).value
+                if congratulation != '':
+                    congratulation_groups[sheet.name].append(congratulation)
+    return congratulation_groups
+
+
 if __name__ == '__main__':
     sheets = load_sheets('x.xls')
     settings = sheets['settings']
     rb = xlrd.open_workbook('x.xls')
     settings = load_settings(settings)
     print(settings)
+    print(sheets)
+
+    congratulations = load_congratulations(filter(lambda sheet: sheet.name != 'settings', sheets.values()))
+    print(congratulations)
 
 
 
